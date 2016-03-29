180e912bc1cbbab2cd77542feadf9d12186c2c05
Isak Andersson <contact@bitpuffin.com>
2016-03-29 14:03:33 +0100

    also build sexpresso_std in windows build script

--- build.bat
+++ build.bat
@@ -2,3 +2,6 @@
 
 call cl /O2 /c sexpresso\sexpresso.cpp
 call lib sexpresso.obj /OUT:sexpresso.lib
+call cl /Isexpresso /O2 /c sexpresso_std\sexpresso_std.cpp
+call lib sexpresso_std.obj /OUT:sexpresso_std.lib
+
